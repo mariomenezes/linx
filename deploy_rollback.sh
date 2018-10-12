14ff6331fc9e5d0f1c1291994c6adc6e6c112421
mariomenezes <mariomenezescosta@gmail.com>
2018-10-11 22:30:25 -0300

    some correction

--- deploy_rollback.sh
+++ deploy_rollback.sh
@@ -18,11 +18,25 @@ cp ecosystem.config.js /home/$USER/app/
 
 cd /home/$USER/app/
 
+#copy ssh credentials. NOTE that is not the best way or the most security
+#but is the only way we can do in this test
+#best way is using ssh-copy-id and append ssh private keys.
+mkdir -p /home/$USER/.ssh/
+cp /home/$USER/linx/ssh_key/id_rsa /home/$USER/.ssh/
+#cp /home/$USER/linx/ssh_key/id_dsa /home/$USER/.ssh/
+
+#Start the ssh-agent in the background.
+eval "$(ssh-agent -s)"
+ssh-add /home/$USER/.ssh/id_rsa
+
+
 # Setup deployment at remote location
 #pm2 deploy production setup
 print "RSA_KEY_PASSWORD: 	123456"
 pm2 deploy ecosystem.config.js production setup
 
+#Deploy code
+pm2 deploy ecosystem.config.js production
 # Update remote version
 pm2 deploy production update
 
@@ -36,15 +50,15 @@ pm2 deploy production exec "pm2 reload all"
 #copy ssh credentials. NOTE that is not the best way or the most security
 #but is the only way we can do in this test
 #best way is using ssh-copy-id and append ssh private keys.
-mkdir -p /home/$USER/.ssh/
-cp /home/$USER/linx/ssh_key/id_rsa /home/$USER/.ssh/
+#mkdir -p /home/$USER/.ssh/
+#cp /home/$USER/linx/ssh_key/id_rsa /home/$USER/.ssh/
 #cp /home/$USER/linx/ssh_key/id_dsa /home/$USER/.ssh/
 
 #Start the ssh-agent in the background.
-eval "$(ssh-agent -s)"
-ssh-add /home/$USER/.ssh/id_rsa
+#eval "$(ssh-agent -s)"
+#ssh-add /home/$USER/.ssh/id_rsa
 
-#Install ssh and dependecies
+#Install ssh and dependecies TODO maybe not necessary
 sudo apt install ssh -y
 #Code to deploy app based on ecosystem.config.js file
 pm2 deploy ecosystem.config.js production setup
