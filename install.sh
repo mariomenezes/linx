e5160f680f236154a37c6dbd369d19e6a9fd37bc
mariomenezes <mariomenezescosta@gmail.com>
2018-10-13 15:15:12 -0300

    Try to solve a problem with default.conf in nginx

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LINX_BASEDIR=$PWD
+_USER=$USER
 
 echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
 . ~/.bashrc
@@ -13,19 +14,29 @@ tar -xzvf node-latest.tar.gz --strip-components 1
 ./configure --prefix=~/local
 make install
 
+echo "performing a profile update..."
+sleep 5
+#. /home/$_USER/.bashrc
+#source /home/$_USER/.profile
+PATH=$PATH:/home/$_USER/local/bin/
+export PATH
+. ~/.bashrc
+
+echo "Init npm and Install express"
+sleep 5
 #Init NPM and create package.json#TODO verify wht it does
 echo "Apenas confirme todas as escolhas"
-/home/$USER/local/bin/npm init
+npm init
 #### Install NPM and Express
-/home/$USER/local/bin/npm install -g express
+npm install -g express
 #Installing the module globally will let you run commands from the command line, but you'll have to link the package into your local sphere to require it from within a program:
-/home/$USER/local/bin/npm link express
+npm link express
 
 #Building and run app.js
 mkdir -p ~/app
 
-#cp /home/$USER/linx/app.js /home/$USER/app/
-cp $LINX_BASEDIR/app.js /home/$USER/app/
+#cp /home/$_USER/linx/app.js /home/$_USER/app/
+cp $LINX_BASEDIR/app.js /home/$_USER/app/
 
 #Install nginx to use reverse proxy
 sudo apt update
@@ -50,17 +61,17 @@ server {\
                         proxy_cache_bypass $http_upgrade;\
                 }\
     location \/static {\
-        root \/home\/'$USER'\/app\/;\
+        root \/home\/'$_USER'\/app\/;\
     }\
 }/1' /etc/nginx/nginx.conf
 
-sudo rm /etc/nginx/sites-enabled/default.conf
-echo -e "\n\n\nBASEDIR: $LINX_BASEDIR\n\n\n"
+#sudo rm /etc/nginx/sites-enabled/default.conf
+#echo -e "\n\n\nBASEDIR: $LINX_BASEDIR\n\n\n"
 #Preparing NGINX server and app.conf
-#sudo cp /home/$USER/linx/app.conf /etc/nginx/sites-available/
-sudo cp $LINX_BASEDIR/app.conf /etc/nginx/sites-available/
-sudo sed -i 's/USER/'$USER'/' /etc/nginx/sites-available/app.conf
-sudo ln -s /etc/nginx/sites-available/app.conf /etc/nginx/sites-enabled/
+#sudo cp /home/$_USER/linx/app.conf /etc/nginx/sites-available/
+#sudo cp $LINX_BASEDIR/app.conf /etc/nginx/sites-available/
+#sudo sed -i 's/USER/'$_USER'/' /etc/nginx/sites-available/app.conf
+#sudo ln -s /etc/nginx/sites-available/app.conf /etc/nginx/sites-enabled/
 sudo cp $LINX_BASEDIR/ssl/nginx-selfsigned.crt /etc/ssl/certs/
 sudo cp $LINX_BASEDIR/ssl/nginx-selfsigned.key /etc/ssl/private/
 sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
@@ -69,22 +80,34 @@ sudo cp $LINX_BASEDIR/ssl/self-signed.conf /etc/nginx/snippets/
 
 #Test and Restart nginx
 echo "Verify the status of the file"
-sleep 3
+sleep 5
 sudo nginx -t 
 sudo service nginx restart
 sudo systemctl enable nginx
+
+sudo rm /etc/nginx/sites-enabled/default.conf
+echo -e "\n\n\nBASEDIR: $LINX_BASEDIR\n\n\n"
+#Preparing NGINX server and app.conf
+#sudo cp /home/$_USER/linx/app.conf /etc/nginx/sites-available/
+sudo cp $LINX_BASEDIR/app.conf /etc/nginx/sites-available/
+sudo sed -i 's/USER/'$_USER'/' /etc/nginx/sites-available/app.conf
+sudo ln -s /etc/nginx/sites-available/app.conf /etc/nginx/sites-enabled/
+
 
 #PM2 -Best way to run in multicore configuration, better than use cluster function
 #Deploy and rollback using PM2
 #pm2 deploy <configuration_file> <environment> <command> 
 #Install pm2
 npm install -g pm2
+sleep 5
 
+echo "Copying express module..."
 #Copy express module to the correct place, so we can run pm2 correctly
-cp -r /home/$USER/local/lib/node_modules/express/ /home/$USER/app/node_modules/
+cp -r /home/$_USER/local/lib/node_modules/express/ /home/$_USER/app/node_modules/
 
+sleep 5
 #Create ecoystem file with PM2 to provide deploy and rollback func
-cd /home/$USER/app/
+cd /home/$_USER/app/
 pm2 ecosystem
 
 #Generate a sample ecosystem.json file that lists the processes and the deployment environment.
@@ -98,31 +121,31 @@ cp ecosystem.config.js ecosystem.config.js.bak
 cd $LINX_BASEDIR
 
 #do some changes in file
-sed -i 's/username/'$USER'/1' ecosystem.config.js
+sed -i 's/username/'$_USER'/1' ecosystem.config.js
 sed -i 's/INSERT_IP/localhost/1' ecosystem.config.js
 sed -i 's/USERNAME/mariomenezes/1' ecosystem.config.js
 sed -i 's/REPOSITORY/linx/1' ecosystem.config.js
-sed -i 's/PATH_TO_APP/\/home\/'$USER'\/app\//1' ecosystem.config.js
+sed -i 's/PATH_TO_APP/\/home\/'$_USER'\/app\//1' ecosystem.config.js
 
-cp ecosystem.config.js /home/$USER/app/
+cp ecosystem.config.js /home/$_USER/app/
 
-cd /home/$USER/app/
+cd /home/$_USER/app/
 
 #copy ssh credentials. NOTE that is not the best way or the most security
 #but is the only way we can do in this test
 #best way is using ssh-copy-id and append ssh private keys.
-mkdir -p /home/$USER/.ssh/
-#cp /home/$USER/linx/ssh_key/id_rsa /home/$USER/.ssh/
-cp $LINX_BASEDIR/ssh_key/id_rsa /home/$USER/.ssh/
+mkdir -p /home/$_USER/.ssh/
+#cp /home/$_USER/linx/ssh_key/id_rsa /home/$_USER/.ssh/
+cp $LINX_BASEDIR/ssh_key/id_rsa /home/$_USER/.ssh/
 
 #Start the ssh-agent in the background.
 eval "$(ssh-agent -s)"
-chmod 0400 /home/$USER/.ssh/id_rsa
+chmod 0400 /home/$_USER/.ssh/id_rsa
 
 echo "INSERT RSA_KEY_PASSWORD WHEN PROMPTED: 123456"
 sleep 3
 
-ssh-add /home/$USER/.ssh/id_rsa
+ssh-add /home/$_USER/.ssh/id_rsa
 
 echo "PM2 - Deploy using github version - possible to do a rolback"
 echo "Deploy code"
@@ -140,12 +163,12 @@ pm2 start app.js -i max
 pm2 list
 
 echo "To ADD app.js in startup"
-echo "sudo env PATH=$PATH:/home/$USER/local/bin /home/$USER/local/lib/node_modules/pm2/bin/pm2 startup app.js -u $USER --hp /home/$USER"
+echo "sudo env PATH=$PATH:/home/$_USER/local/bin /home/$_USER/local/lib/node_modules/pm2/bin/pm2 startup app.js -u $_USER --hp /home/$_USER"
 
 #tarefa
-mkdir -p /home/$USER/cron_job
-cp $LINX_BASEDIR/envia_relatorio.sh /home/$USER/cron_job/
-crontab -l ; echo -e "MAILTO="mario@linx.intra"\n@daily /home/$USER/cron_job/envia_relatorio.sh" | crontab
+mkdir -p /home/$_USER/cron_job
+cp $LINX_BASEDIR/envia_relatorio.sh /home/$_USER/cron_job/
+crontab -l ; echo -e "MAILTO="mario@linx.intra"\n@daily /home/$_USER/cron_job/envia_relatorio.sh" | crontab
 
 #Throughput test using siege -  a command line load test tool
 echo "starting test with 100 concurrent request - duration 10s"
